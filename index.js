ed75dd61e1593020341284b4fc674c3ffe1617c2
nusraht <nusraht.khaledur@globeair.com>
2023-02-21 21:52:58 +0100

    route

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+
+///////////////////////////////////////////
 //blocking synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt","utf-8");
 // console.log(textIn);
@@ -9,16 +12,43 @@ const http = require('http');
 // console.log('File written');
 
 //non-blocking asynchronous way
- fs.readFile("./txt/start.txt","utf-8",(err,data1) => {
-    if (err) return console.log("error");
-    fs.readFile(`./txt/${data1}.txt`,"utf-8",(err,data2) => {
-        console.log(data2);
-        fs.readFile("./txt/append.txt","utf-8",(err,data3) => {
-            console.log(data3);
+//  fs.readFile("./txt/start.txt","utf-8",(err,data1) => {
+//     if (err) return console.log("error");
+//     fs.readFile(`./txt/${data1}.txt`,"utf-8",(err,data2) => {
+//         console.log(data2);
+//         fs.readFile("./txt/append.txt","utf-8",(err,data3) => {
+//             console.log(data3);
 
-            fs.writeFile("./txt/final.txt",`${data2}\n${data3}`,"utf-8", err => {
-                console.log('File written');
-            })
-         });
-     });
- })
+//             fs.writeFile("./txt/final.txt",`${data2}\n${data3}`,"utf-8", err => {
+//                 console.log('File written');
+//             })
+//          });
+//      });
+//  })
+///////////////////////////////////////////
+
+//SERVER
+const server = http.createServer((req,res) => {
+    const pathname  = req.url;
+    // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    
+    res.end('overview');
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.end('product');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+
+    res.end('Hello from server');
+});
+
+server.listen(8000,'127.0.0.1', () => {
+    console.log('listening');
+});
